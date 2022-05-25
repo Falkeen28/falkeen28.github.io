99ff901ae781cb105636cefa8f07fbc2ff1cb41b
Kacper <105564535+Falkeen28@users.noreply.github.com>
2022-05-25 17:49:03 +0200

    .

--- js/BMI.js
+++ js/BMI.js
@@ -1,20 +1,17 @@
 function bmi(x, y) 
         {
-            if ((x / (y * y)) >= 40) return((x / (y * y))) + " czyli otyłość III stopnia";
-            else if ((x / (y * y)) > 35 && (x / (y * y)) < 39.99 ) return((x / (y * y))) + " czyli otyłość II stopnia";
-            else if ((x / (y * y)) > 30 && (x / (y * y)) < 34.99 ) return((x / (y * y))) + " czyli otyłość I stopnia";
-            else if ((x / (y * y)) > 25 && (x / (y * y)) < 29.99) return((x / (y * y))) + " czyli Nadwaga";
-            else if ((x / (y * y)) > 18.5 && (x / (y * y)) < 24.99 ) return((x / (y * y))) + " czyli Waga prawidłowa";
-            else if ((x / (y * y)) > 17 && (x / (y * y)) < 18.49 ) return((x / (y * y))) + " czyli Niedowaga";
-            else if ((x / (y * y)) > 16 && (x / (y * y)) < 16.99 ) return((x / (y * y))) + " czyli Wychudzenie";
-            else if ((x / (y * y)) < 16 ) return((x / (y * y))) + " czyli Wyglodzenie";
+            if ((x / (y * y)) >= 35) return((x / (y * y)));
+            else if ((x / (y * y)) > 30 && (x / (y * y)) < 34.99) return((x / (y * y)));
+            else if ((x / (y * y)) > 25 && (x / (y * y)) < 29.99) return((x / (y * y)));
+            else if ((x / (y * y)) > 18.5 && (x / (y * y)) < 24.99) return((x / (y * y)));
+            else if ((x / (y * y)) < 18.5) return((x / (y * y)));
         }
         function oblicz() 
         {
             var x = document.getElementById("waga").value, y = document.getElementById("wzrost").value;
             if (isNaN(y)) 
             {
-                alert("Podaj wzrost w centymetrach");
+                alert("Podaj wzrost w metrach");
                 console.log("y", y);
             }
             else if ( y > 2.40 )
@@ -43,5 +40,11 @@ function bmi(x, y)
                 console.log("x", x);
             }
             else
-                document.getElementById("odpowiedz").innerHTML = "BMI wynosi " + bmi(x, y);
+                document.getElementById("odpowiedz").innerHTML = "BMI wynosi " + Number(Math.round(bmi(x, y) + 'e+2') + 'e-2'); 
+                //za pomocą notacji wykładniczej zaokrągliłem liczbe do 2 miejsc po przecinku :D
+                
         }
+       /* function obrazek()
+        {   if(Number(Math.round(bmi(x, y) + 'e+2') + 'e-2') > 25  && Number(Math.round(bmi(x, y) + 'e+2') + 'e-2') < 18.5)
+                document.getElementById("obrazek").addEventListener("click", oblicz(){obslugazdarzenia("4A ")});  
+        }*/
